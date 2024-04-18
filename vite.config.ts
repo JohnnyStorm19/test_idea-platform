5238066530168dcbc247f4a21e7322bda07245c5
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2024-04-18 19:14:26 +0300

    config: remove base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: 'https://johnnystorm19.github.io/test_idea-platform/'
+  plugins: [react()]
 })
