29106f5f584435cca2b46f1e4058d4edfd299475
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2024-04-18 19:36:10 +0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: 'https://johnnystorm19.github.io/test_idea-platform/'
 })
