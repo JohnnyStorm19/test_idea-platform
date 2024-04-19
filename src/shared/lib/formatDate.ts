df6435636121b5b235cb5e5457d6d14ecd172eeb
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2024-04-19 11:11:35 +0300

    refactor: import day js

--- src/shared/lib/formatDate.ts
+++ src/shared/lib/formatDate.ts
@@ -1,10 +1,10 @@
-import * as dayjs from "dayjs";
-import 'dayjs/locale/ru';
-import customParseFormat from 'dayjs/plugin/customParseFormat'
-dayjs.extend(customParseFormat)
-dayjs.locale('ru')
+import dayjs from "dayjs";
+import "dayjs/locale/ru";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+dayjs.extend(customParseFormat);
+dayjs.locale("ru");
 
 export const formatDate = (dateStr: string) => {
-  const date = dayjs(`${dateStr}`, 'DD.MM.YY');
-  return date.format('D MMM YYYY, dd');
+  const date = dayjs(`${dateStr}`, "DD.MM.YY");
+  return date.format("D MMM YYYY, dd");
 };
